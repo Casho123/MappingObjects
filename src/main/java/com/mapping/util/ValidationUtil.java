58e989dc17449b765862a36d2039c87cc852ebd8
My PC <alex.petkov606@gmail.com>
2023-03-20 15:16:43 +0200

    asd

--- src/main/java/com/mapping/util/ValidationUtil.java
+++ src/main/java/com/mapping/util/ValidationUtil.java
@@ -1,10 +1,14 @@
 package com.mapping.util;
 
+
 import jakarta.validation.ConstraintViolation;
 
 import java.util.Set;
 
 public interface ValidationUtil {
 
+
+    <E> boolean isValid(E entity);
     <E> Set<ConstraintViolation<E>> violation(E entity);
+
 }
