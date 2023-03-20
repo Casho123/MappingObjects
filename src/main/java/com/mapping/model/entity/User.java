bd2c2c2c9da28430f094f59b134ffd5c79d8db78
My PC <alex.petkov606@gmail.com>
2023-03-20 14:43:12 +0200

    asd

---
+++ src/main/java/com/mapping/model/entity/User.java
@@ -0,0 +1,58 @@
+package com.mapping.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "Users")
+public class User extends BaseEntity {
+
+    @Column
+    private String email;
+    @Column
+    private String password;
+    @Column(name = "full_name")
+    private String fullName;
+
+    @OneToMany
+    private Set<Game> games;
+
+    public User() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Set<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(Set<Game> games) {
+        this.games = games;
+    }
+}
