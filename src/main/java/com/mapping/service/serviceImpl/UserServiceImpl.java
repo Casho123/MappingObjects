9e792df34071073fe7b628f978bd6a671d9f447c
My PC <alex.petkov606@gmail.com>
2023-03-20 15:49:15 +0200

    asd

---
+++ src/main/java/com/mapping/service/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.mapping.service.serviceImpl;
+
+
+import com.mapping.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
